e2aff1cee792c046f461aa56d0c04ba490835e23
Jean Guillaume <jgi@jgwill.com>
2020-05-22 17:15:57 -0400

    practice(typing): created a python model using QuickType

---
+++ model_typing/person_test.py
@@ -0,0 +1,9 @@
+import person
+
+telList=[person.PhoneNumberPhoneNumber(["home"],"5143121378"),person.PhoneNumberPhoneNumber(["mobile"],"8195346604")]
+
+#person.List(person.PhoneNumberPhoneNumber(["home"],"5143121378"),person.PhoneNumberPhoneNumber(["mobile","8195346604"]))
+
+moi= person.PersonClass("Jean Guillaume","Isabelle",44,"6830, Rue Boyer",telList,[person.Sex(["m"])])
+
+print_r(moi)
